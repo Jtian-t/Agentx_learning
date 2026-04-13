737693b77b6853c1f2c3f8fc0a5fed36ba7b26d2
Jtian-t <1181121620@qq.com>
2026-04-13 13:59:53 +0800

    1 版本初始化

---
+++ src/main/java/com/jin/agentx_learning/AgentXLearningApplication.java
@@ -0,0 +1,13 @@
+package com.jin.agentx_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentXLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentXLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jin/agentx_learning/AgentXLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.agentx_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentXLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
